c6cb3ccae75a4e4d01aa6cf2243fa2270303f7ea
Александр Лихих <liha@gkomega.ru>
2021-03-10 09:45:34 +0300

    + google sheets api

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,19 @@
-import { ApolloServer } from "apollo-server";
+import {ApolloServer} from "apollo-server";
+import gapiToGraphQL from 'gapi-to-graphql'
+import GoogleSheetsAPI from 'gapi-to-graphql/google_apis/sheets-v4';
+import {gql} from "apollo-server";
 
-import { BooksProvider } from "./provider";
-import { resolvers, typeDefs } from "./resolver";
+const {schema, resolvers} = gapiToGraphQL({gapiAsJsonSchema: GoogleSheetsAPI})
 
-// This is where we define the context type which is used
-// to have correct typing when using context in the resolvers.
-export interface Context {
-  dataSources: {
-    booksProvider: BooksProvider;
-  };
-}
 
-// This is where we define the dataSources which can be
-// used to retrieve data from the resolvers.
-const dataSources = (): Context["dataSources"] => {
-  return {
-    booksProvider: new BooksProvider()
-  };
-};
-
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({
-  typeDefs,
-  // @ts-ignore (FIXME: should be casted to default Resolvers type?)
-  resolvers,
-  dataSources
+    typeDefs: gql`
+        ${schema}
+    `,
+    // @ts-ignore (FIXME: should be casted to default Resolvers type?)
+    resolvers,
 });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`); // tslint:disable-line no-console
+server.listen().then(({url}) => {
+    console.log(`🚀  Server ready at ${url}`); // tslint:disable-line no-console
 });
